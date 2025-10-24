b84a040904670bf303d1cb19b8b7849f6bc9733e
jquitoy <jquitoy@filamer.edu.ph>
2025-10-24 17:17:15 +0800

    routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route:: get('/greet/{name}', action: function($name): string(
+//     return "Hello, $name"
+// )
+
+// )
+
+Route::get('/sum/{x}/{y}', action: function($x, $y): string{
+    $sum = $x + $y;
+    return "The sum of the $x and $y is $sum";
+});
